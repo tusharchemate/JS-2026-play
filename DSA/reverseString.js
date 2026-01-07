6384f15ea099a6fae00caec0088a868590bb5bbc
Tushar Chemate <41479583+tusharchemate@users.noreply.github.com>
2026-01-07 17:06:32 +0000

    reverse string

---
+++ DSA/reverseString.js
@@ -0,0 +1,11 @@
+const string = "tushar chemate";
+
+const reverseString = (str)=> {
+   let rev="";
+   for(let i=str.length-1; i>=0; i--) {
+      rev += str[i];
+   }
+   return rev;
+}
+
+console.log(reverseString(string));
